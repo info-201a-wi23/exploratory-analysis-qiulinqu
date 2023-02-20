a36ec31cb08b0b0725394d5428f09279efd57288
naimamohaba <naimamohaba0292@gmail.com>
2023-02-19 19:01:30 -0800

    chart3

---
+++ chart 3.R
@@ -0,0 +1,17 @@
+library(dplyr)
+library(ggplot2)
+
+food_data <- read.csv("Food_Supply_kcal_Data.csv")
+#wWhat are the top 10 countries with the most deaths from food malnutrition?
+deaths_data <- food_data %>% select(Country, Deaths)
+
+deaths_data1 <- deaths_data %>% arrange(desc(Deaths))
+
+deaths_top10 <- deaths_data1[1:10,]
+
+ggplot(deaths_top10, aes(x = Country, y = Deaths)) +
+  geom_bar(stat = "identity", fill = "blue") +
+  xlab("Country") +
+  ylab("Number of deaths") +
+  ggtitle("Top 10 countries with the most deaths")
+           
