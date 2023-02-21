743329733554abef281d3c1ccda18d914816bcec
EmmsFin <emmafin@uw.edu>
2023-02-20 18:34:35 -0800

    finished summary

--- chart2.R
+++ chart2.R
@@ -27,3 +27,8 @@ undernourished_to_death <- ggplot(undernourished_deaths_df) +
 
 # display map
 undernourished_to_death
+
+
+# write a summary paragraph
+
+# This chart examines the relationship between undernourishment and death from COVID-19. The data shows that both lower and higher undernourished rates are correlated with low deaths and that both low to medium undernourishment rates are correlated with higher death rates. From this observation of both high and low undernourishment values bundled together, we can infer that there is no correlation between a country having a high undernourishment percent and a country having an increased percent of death's.This information seems contrary to everything I have learned, when a person is undernourished their ability to fight off sickness is decreased, thus I would have predicted that higher rates of undernourishment would correlate with higher rates of undernourishment. This broaches the question, does undernourishment affect the amount of confirmed cases? If so, why does it affect confirmed cases and not deaths? If it doesn't, the question becomes, why does undernourishment not impact COVID-19 rates, when it does impact the immune system?
